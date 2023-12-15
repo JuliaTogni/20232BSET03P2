fd3504189cabba3580e8c93d41dfdf9bb003cee2
JuliaTogni <julia.togni@sou.inteli.edu.br>
2023-12-15 14:53:17 -0300

    ProvaJu

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+
+### Vulnerabilidades identificadas e as medidas adotadas para corrigir
+
+
+#### Injeção de SQL
+O código original não utiliza placeholders para inserir os valores nas consultas SQL, permitindo a possibilidade de ataques de injeção de SQL. Um invasor mal-intencionado poderia explorar essa vulnerabilidade para executar comandos SQL não autorizados no banco de dados.
+
+- Medida adotada para corrigir:
+Foi utilizada a função ```db.run``` com placeholders e parâmetros para executar as consultas SQL. Evitando a injeção de SQL, pois os valores são tratados como dados e não como parte do comando SQL.
+
+#### Validação ID inexistente: 
+O código original não verificava se o ID fornecido existia na tabela correspondente (cats ou dogs). Isso poderia permitir que um ID inválido fosse usado na consulta de atualização, resultando em uma atualização incorreta ou em um erro no banco de dados.
+- Medida adotada para corrigir: Foi adicionada uma consulta SELECT antes da atualização dos votos para verificar se o ID existe na tabela correspondente (cats ou dogs). Caso o ID não seja encontrado, é retornado um status 404 indicando que o ID não existe.
+
+#### Validação tipo de animal: 
+O código original também não validava corretamente o tipo de animal fornecido (cats ou dogs). Isso poderia permitir que um tipo de animal inválido fosse usado na consulta de atualização, resultando em uma atualização incorreta ou em um erro no banco de dados.
+- Medida adotada para corrigir: Foi adicionada uma verificação para garantir que o tipo de animal fornecido seja válido. Se o tipo de animal não for "cats" nem "dogs", é retornado um status 400 indicando que o tipo de animal é inválido.
+
+
+#### Validação de dados: 
+O código original não realiza uma validação adequada dos dados de entrada, permitindo que valores inválidos ou vazios sejam inseridos no banco de dados.
+
+- Medida adotada para corrigir:
+Foi adicionada a validação dos dados de entrada utilizando o pacote ```express-validator```. Foram adicionadas regras de validação para garantir que o campo "name" não esteja vazio antes de inserir os dados no banco de dados.
+
+#### Melhoria e padronização das respostas de erro: 
+O código original já retornava respostas de erro (status 500), mas não fornecia informações detalhadas sobre o erro que ocorreu. E também, o código original retornava respostas de sucesso com status 200 para uma rota e status 201 para outra rota.
+- Medida adotada para corrigir a vulnerabilidade: Foram adicionadas respostas de erro descritivas, fornecendo mensagens de erro e códigos de status HTTP padronizados.
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 const port = 3000;
@@ -11,11 +12,15 @@ app.use(bodyParser.json());
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
 });
 
-app.post('/cats', (req, res) => {
+app.post('/cats', [body("name").notEmpty().withMessage("O nome é obrigatório")], (req, res) => {
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
   const name = req.body.name;
   db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
     if (err) {
@@ -26,14 +31,42 @@ app.post('/cats', (req, res) => {
   });
 });
 
-app.post('/dogs', (req, res) => {
-  
+app.post('/dogs', [body("name").notEmpty().withMessage("O nome é obrigatório")], (req, res) => {
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const name = req.body.name;
+    db.run(`INSERT INTO dogs (name, votes) VALUES ('${name}', 0)`, function (err) {
+        if (err) {
+          res.status(500).send("Erro ao inserir no banco de dados");
+        } else {
+          res.status(201).json({ id: this.lastID, name, votes: 0 });
+        }
+      });
 });
 
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+  const { animalType, id } = req.params;
+  if (animalType !== "cats" && animalType !== "dogs") {
+    return res.status(400).send("Tipo de animal inválido"
+  )};
+  const query = `SELECT * FROM ${animalType} WHERE id = ?`;
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else if (!row) {
+      res.status(404).send("Animal não encontrado");
+    }else {
+      db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`, function (err) {
+        if (err) {
+          res.status(500).send("Erro ao atualizar os votos");
+        } else {
+          res.status(200).send("Voto computado");
+        }
+      });
+    }
+  });
 });
 
 app.get('/cats', (req, res) => {
@@ -47,7 +80,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
